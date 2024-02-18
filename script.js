3b1c067fd01fdb2e6bb7dcc11bebb39b0a6c68e5
Vipin Kumar <vkprajapati665@gmail.com>
2024-02-18 22:53:04 +0530

    fix: restart fix

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 const inputContainer = document.querySelector(".input-container");
 const infoContainer = document.querySelector(".info-container");
 const submitBtn = document.getElementById('submit');
+const resetBtn = document.getElementById('reset');
 infoContainer.style.display = "none";
+resetBtn.style.display = "none";
 
-submitBtn.addEventListener('click' , async function fetchDataCF(){
-    const inputValue = document.getElementById('userName1').value;
+async function fetchDataCF(){
+    inputValue = document.getElementById('userName1').value;
     if (inputValue.trim() === "") {
         document.getElementById("warningMessage").innerHTML = "Please enter username";
         return false;
     } else {
         inputContainer.style.display = "none";
         infoContainer.style.display = "block";
+        resetBtn.style.display = "block";
         try {
+            localStorage.setItem("uname",inputValue);
             const fetchData = await fetch(`https://codeforces.com/api/user.rating?handle=${inputValue}`);
             const data = await fetchData.json();
             let rating = data["result"][data["result"].length - 1]["newRating"];
@@ -45,6 +49,23 @@ submitBtn.addEventListener('click' , async function fetchDataCF(){
             document.getElementById("warningMessage2").innerHTML = "Error fetching data. Please try again later.";
         }
     }
+}
 
+submitBtn.addEventListener('click' , fetchDataCF);
+resetBtn.addEventListener('click', resetLocalStorage);
+
+window.addEventListener("load", () => {
+    const storedUsername = localStorage.getItem("uname");
+    if (storedUsername) {
+        document.getElementById('userName1').value = storedUsername;
+        fetchDataCF(); // Fetch data using the stored username
+    }
 });
+
+function resetLocalStorage() {
+    localStorage.removeItem("uname");
+    document.getElementById('userName1').value = "";
+    location.reload();
+}
+
 
